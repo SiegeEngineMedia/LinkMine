845e9ecd362c96565c440cadb12222209862ede7
CygnusRoboticus <joshuadaleharris@gmail.com>
2022-07-17 17:45:03 -0400

    initial commit

---
+++ src/main/java/com/sem/linkmine/LinkMineApplication.java
@@ -0,0 +1,13 @@
+package com.sem.linkmine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class LinkMineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LinkMineApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sem/linkmine/LinkMineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sem.linkmine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkMineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
